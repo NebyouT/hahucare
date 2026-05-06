629bf50a9e832538ee97f841ed23e8d582a93e71
NebyouT <nebyoutilikalem@gmail.com>
2026-05-06 14:50:57 +0300

    fix route order for advanced referral

--- Modules/PatientReferral/Routes/web.php
+++ Modules/PatientReferral/Routes/web.php
@@ -21,39 +21,7 @@ Route::group(['prefix' => 'app', 'as' => 'backend.', 'middleware' => ['auth', 'a
         ->name('patientreferral.index')
         ->middleware('permission:view_patient_referral');
         
-    Route::get('patientreferral/create', [PatientReferralController::class, 'create'])
-        ->name('patientreferral.create')
-        ->middleware('permission:add_patient_referral');
-        
-    Route::post('patientreferral', [PatientReferralController::class, 'store'])
-        ->name('patientreferral.store')
-        ->middleware('permission:add_patient_referral');
-        
-    Route::get('patientreferral/{id}/edit', [PatientReferralController::class, 'edit'])
-        ->name('patientreferral.edit')
-        ->middleware('permission:edit_patient_referral');
-        
-    Route::put('patientreferral/{id}', [PatientReferralController::class, 'update'])
-        ->name('patientreferral.update')
-        ->middleware('permission:edit_patient_referral');
-        
-    Route::delete('patientreferral/{id}', [PatientReferralController::class, 'destroy'])
-        ->name('patientreferral.destroy')
-        ->middleware('permission:delete_patient_referral');
-        
-    Route::get('patientreferral/{id}', [PatientReferralController::class, 'show'])
-        ->name('patientreferral.show')
-        ->middleware('permission:view_patient_referral');
-        
-    Route::post('patientreferral/{id}/accept', [PatientReferralController::class, 'acceptReferral'])
-        ->name('patientreferral.accept')
-        ->middleware('permission:edit_patient_referral');
-        
-    Route::get('patientreferral/{id}/book', [PatientReferralController::class, 'bookAppointment'])
-        ->name('patientreferral.book')
-        ->middleware('permission:edit_patient_referral');
-
-    // Advanced Referral Routes
+    // Advanced Referral Routes - MUST be before {id} routes to avoid conflicts
     Route::get('patientreferral/create-advanced', [PatientReferralController::class, 'createAdvanced'])
         ->name('patientreferral.create-advanced')
         ->middleware('permission:add_patient_referral');
@@ -68,6 +36,39 @@ Route::group(['prefix' => 'app', 'as' => 'backend.', 'middleware' => ['auth', 'a
         
     Route::put('patientreferral/{id}/update-advanced', [PatientReferralController::class, 'updateAdvanced'])
         ->name('patientreferral.update-advanced')
+        ->middleware('permission:edit_patient_referral');
+
+    // Standard Referral Routes
+    Route::get('patientreferral/create', [PatientReferralController::class, 'create'])
+        ->name('patientreferral.create')
+        ->middleware('permission:add_patient_referral');
+        
+    Route::post('patientreferral', [PatientReferralController::class, 'store'])
+        ->name('patientreferral.store')
+        ->middleware('permission:add_patient_referral');
+        
+    Route::get('patientreferral/{id}', [PatientReferralController::class, 'show'])
+        ->name('patientreferral.show')
+        ->middleware('permission:view_patient_referral');
+        
+    Route::get('patientreferral/{id}/edit', [PatientReferralController::class, 'edit'])
+        ->name('patientreferral.edit')
+        ->middleware('permission:edit_patient_referral');
+        
+    Route::put('patientreferral/{id}', [PatientReferralController::class, 'update'])
+        ->name('patientreferral.update')
+        ->middleware('permission:edit_patient_referral');
+        
+    Route::delete('patientreferral/{id}', [PatientReferralController::class, 'destroy'])
+        ->name('patientreferral.destroy')
+        ->middleware('permission:delete_patient_referral');
+        
+    Route::post('patientreferral/{id}/accept', [PatientReferralController::class, 'acceptReferral'])
+        ->name('patientreferral.accept')
+        ->middleware('permission:edit_patient_referral');
+        
+    Route::get('patientreferral/{id}/book', [PatientReferralController::class, 'bookAppointment'])
+        ->name('patientreferral.book')
         ->middleware('permission:edit_patient_referral');
 
     // PDF Download Route
