e5c5255873cb6680c779b238418be17ffbb54cbd
NebyouT <nebyoutilikalem@gmail.com>
2026-06-29 17:50:51 +0300

    api 404 problem fixed

--- Modules/PatientReferral/Routes/api.php
+++ Modules/PatientReferral/Routes/api.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Modules\PatientReferral\Http\Controllers\API\PatientReferralAPIController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
-    Route::get('patient-referrals', [PatientReferralAPIController::class, 'index'])->name('patientreferral.list');
-    Route::get('patient-referral-detail', [PatientReferralAPIController::class, 'show'])->name('patientreferral.detail');
-});
+Route::get('patient-referrals', [PatientReferralAPIController::class, 'index'])->middleware('auth:sanctum');
+Route::get('patient-referral-detail', [PatientReferralAPIController::class, 'show'])->middleware('auth:sanctum');
